01625da44d8fe00c20d6229086205175f586cff0
ImInYourPie <iminyourpie@gmail.com>
2020-06-18 22:11:00 +0100

    Lol

---
+++ index.js
@@ -0,0 +1,9 @@
+Reveal.initialize({
+  hash: true,
+  plugins: [RevealMarkdown, RevealHighlight, RevealNotes],
+  controlsTutorial: true,
+  slideNumber: false,
+  showSlideNumber: "all",
+  center: false,
+  transition: "concave"
+})
